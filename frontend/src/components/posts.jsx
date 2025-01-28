2384cfd668a96d0a231c9c459f10f1a4f27866db
Sonu Lodhi <sp95759201@gmail.com>
2025-01-28 22:14:47 +0530

    clear

--- frontend/src/components/posts.jsx
+++ frontend/src/components/posts.jsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 
 const Posts = () => {
-    const [data, SetData] = useState()
-
-    const handleChange = () => {
-        const {name, value} = e.target;
-        setvalues({...data, [name]: value})
-    }
+    
+    
   return (
     <div>Posts
         <h1>Posts</h1>
