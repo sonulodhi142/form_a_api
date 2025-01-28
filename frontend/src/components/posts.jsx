ec6bfc60162e89fff1491f58e23d6064d7776ea1
Sonu Lodhi <sp95759201@gmail.com>
2025-01-28 22:13:58 +0530

    take form data

--- frontend/src/components/posts.jsx
+++ frontend/src/components/posts.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Posts = () => {
+    const [data, SetData] = useState()
+
+    const handleChange = () => {
+        const {name, value} = e.target;
+        setvalues({...data, [name]: value})
+    }
   return (
     <div>Posts
         <h1>Posts</h1>
